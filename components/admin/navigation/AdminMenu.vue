0100ad31952dfcc4c953f176b3bb9104067c59db
serii <seriiburduja@gmail.com>
2022-11-16 18:50:39 +0200

    feat: post admin menu

--- components/admin/navigation/AdminMenu.vue
+++ components/admin/navigation/AdminMenu.vue
@@ -2,6 +2,7 @@
   ul.admin-menu
     AdminMenuItem(label='Dashboard', icon='/svg/dashboard.svg', url='/admin', :link='true')
     AdminMenuItem(label='Post category', icon='/svg/category.svg', :items="[{ url: '/admin/post-category/create' }, { url: '/admin/post-category' }]")
+    AdminMenuItem(label='Post', icon='/svg/category.svg', :items="[{ url: '/admin/post/create' }, { url: '/admin/post' }]")
 </template>
 <script>
 import AdminMenuItem from "./AdminMenuItem.vue";
---
+++ pages/admin/post/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <AdminForm label="Post categories">
+    <AdminTable>
+      <div class="search">
+        <label htmlFor="search">Search:</label>
+        <input type="text" v-model="search" @input="onSearch" />
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Slug</th>
+            <th>Category</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in data" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>
+              <img
+                :src="`${server_url}${item.image}`"
+                width="100"
+                alt=""
+              />
+            </td>
+            <td>{{ item.title }}</td>
+            <td>{{ item.slug }}</td>
+            <td>{{ item.category }}</td>
+            <td>
+              <span class="badge" :class="badgeClass(item)">
+                {{ item.status }}
+              </span>
+            </td>
+            <td>
+              <nuxt-link :to="`/admin/post-category/` + item.id">
+                <a class="btn btn--success">Edit</a>
+              </nuxt-link>
+              <button
+                class="btn btn--danger"
+                @click="() => deleteItem(item.id)"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </AdminTable>
+  </AdminForm>
+</template>
+<script>
+import AdminForm from "@/admin/form/Form.vue";
+import AdminTable from "@/admin/form/AdminTable.vue";
+export default {
+  layout: "admin",
+  data() {
+    return {
+      search: "",
+      data: [],
+    };
+  },
+  components: {
+    AdminForm,
+    AdminTable,
+  },
+  computed: {
+    server_url() {
+      return this.$store.state.server_url;
+    },
+  },
+  methods: {
+    onSearch() {
+      this.getData();
+    },
+    badgeClass(item) {
+      return item.status == "1" ? "badge--success" : "badge--danger";
+    },
+    formatDate(date) {
+      let options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: false,
+      };
+
+      return new Intl.DateTimeFormat("en", options).format(new Date(date));
+    },
+    deleteItem(id) {
+      this.$axios
+        .delete("/auth/post/" + id)
+        .then(() => {
+          this.getData();
+        })
+        .catch((err) => {
+          console.log(err.response.data.message, "err.response");
+        });
+    },
+    getData() {
+      this.$axios
+        .get("/auth/post")
+        .then((res) => {
+          this.data = res.data.data;
+          console.log(this.data, "this.data");
+        })
+        .catch((err) => {
+          console.log(err.response.data.message, "err.response");
+        });
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
