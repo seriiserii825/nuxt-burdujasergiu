608a42e4b5cb6a6498638eb6dd13e71144b74e9b
serii <seriiburduja@gmail.com>
2022-11-18 09:54:00 +0200

    feat: crud taxonomy

--- components/admin/navigation/AdminMenu.vue
+++ components/admin/navigation/AdminMenu.vue
@@ -3,6 +3,7 @@
     AdminMenuItem(label='Dashboard', icon='/svg/dashboard.svg', url='/admin', :link='true')
     AdminMenuItem(label='Post category', icon='/svg/category.svg', :items="[{ url: '/admin/post-category/create' }, { url: '/admin/post-category' }]")
     AdminMenuItem(label='Post', icon='/svg/category.svg', :items="[{ url: '/admin/post/create' }, { url: '/admin/post' }]")
+    AdminMenuItem(label='Taxonomy', icon='/svg/category.svg', :items="[{ url: '/admin/taxonomy/create' }, { url: '/admin/taxonomy' }]")
 </template>
 <script>
 import AdminMenuItem from "./AdminMenuItem.vue";
---
+++ pages/admin/taxonomy/create.vue
@@ -0,0 +1,48 @@
+<template>
+  <AdminForm label="Create book">
+    <div class="form__flex">
+      <div class="form__item" :class="{ 'form__item--error': errors.name }">
+        <label class="form__label" htmlFor="name">Title</label>
+        <input type="text" placeholder="Enter title..." v-model="title" />
+        <p v-if="errors && errors.title" class="text-error">
+          {{ errors.title[0] }}
+        </p>
+      </div>
+    </div>
+    <button class="btn" @click="onSubmit">Submit</button>
+  </AdminForm>
+</template>
+<script>
+import AdminForm from "@/admin/form/Form";
+export default {
+  layout: "admin",
+  data() {
+    return {
+      title: "",
+      errors: {},
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      const data = {
+        title: this.title,
+      };
+
+      this.$axios
+        .post("/auth/taxonomy", data)
+        .then(() => {
+          this.$router.push("/admin/taxonomy");
+        })
+        .catch((err) => {
+          if (err.response.data && err.response.data.errors) {
+            this.errors = err.response.data.errors;
+          }
+        });
+    },
+  },
+  components: {
+    AdminForm,
+  },
+};
+</script>
