32522065a5afbbe2db1c8966dc00561eb0a198ba
serii <seriiburduja@gmail.com>
2022-11-20 09:42:42 +0200

    feat: ecosystem.config.js changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  apps : [
+  apps: [
     {
-      name: "nuxt-dev",
-      script: "npm",
-      args: "run dev"
-    },
-    {
-      name: "nuxt-prod",
-      script: "npm",
-      args: "run start"
+      name: 'NuxtAppName',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
     }
   ]
 }
