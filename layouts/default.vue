54f27160ac49a7efab8a13167032600ffe736b68
serii <seriiburduja@gmail.com>
2022-12-03 19:42:40 +0200

    searchPopop: finished

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
   .default
+    SearchPopup(v-if="is_visible_popup" @hide="hideSearch")
+    .show-search(@click="showSearch")
+      img(src="/svg/search.svg" width="50" alt="")
     .default__wrap
       .default__menu
         .default__menu-logo
@@ -13,6 +16,7 @@
 import Menu from "../components/Menu";
 import Logo from "../components/Logo";
 import Footer from "../components/Footer";
+import SearchPopup from "@/SearchPopup";
 
 export default {
   head() {
@@ -22,10 +26,31 @@ export default {
       ]
     }
   },
+  data() {
+    return {
+      is_visible_popup: false
+    }
+  },
+  methods: {
+    showSearch() {
+      this.is_visible_popup = true;
+    },
+    hideSearch() {
+      this.is_visible_popup = false;
+    }
+  },
   components: {
     Menu,
     Logo,
-    Footer
+    Footer,
+    SearchPopup
+  },
+  mounted() {
+    document.addEventListener('keyup', (e) => {
+      if (e.code === 'Escape' && this.is_visible_popup) {
+        this.hideSearch();
+      }
+    });
   }
 }
 </script>
@@ -49,6 +74,17 @@ export default {
   }
   &__content {
     flex: 1;
+  }
+}
+.show-search {
+  position: fixed;
+  top: 3rem;
+  right: 3rem;
+  cursor: pointer;
+  transition: all .4s;
+  z-index: 99999;
+  &:hover {
+    transform: scale(0.9);
   }
 }
 </style>
