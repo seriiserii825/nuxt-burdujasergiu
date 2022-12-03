208081ffbae89e66ebf4ca1e4eb1075d0f8b80f8
serii <seriiburduja@gmail.com>
2022-12-03 11:06:44 +0200

    services: from js file

--- pages/services.vue
+++ pages/services.vue
@@ -1,42 +1,28 @@
 <template>
   <div class="page-services">
-      <section-header title="Мои преимущества" subtitle="Услуги"/>
-      <div class="page-services__wrap">
-        <div class="page-services__item">
-          <Service url="https://ru.vuejs.org/" title="Vue" text="Vue (произносится /vjuː/, примерно как view) — это прогрессивный фреймворк для создания пользовательских интерфейсов. В отличие от фреймворков-монолитов, Vue создан пригодным для постепенного внедрения.">
-            <img src="/svg/vue.svg" alt="">
-          </Service>
-        </div>
-        <div class="page-services__item">
-          <Service url="https://ru.vuejs.org/" title="Vue" text="Vue (произносится /vjuː/, примерно как view) — это прогрессивный фреймворк для создания пользовательских интерфейсов. В отличие от фреймворков-монолитов, Vue создан пригодным для постепенного внедрения.">
-            <img src="/svg/vue.svg" alt="">
-          </Service>
-        </div>
-        <div class="page-services__item">
-          <Service url="https://ru.vuejs.org/" title="Vue" text="Vue (произносится /vjuː/, примерно как view) — это прогрессивный фреймворк для создания пользовательских интерфейсов. В отличие от фреймворков-монолитов, Vue создан пригодным для постепенного внедрения.">
-            <img src="/svg/vue.svg" alt="">
-          </Service>
-        </div>
-        <div class="page-services__item">
-          <Service url="https://ru.vuejs.org/" title="Vue" text="Vue (произносится /vjuː/, примерно как view) — это прогрессивный фреймворк для создания пользовательских интерфейсов. В отличие от фреймворков-монолитов, Vue создан пригодным для постепенного внедрения.">
-            <img src="/svg/vue.svg" alt="">
-          </Service>
-        </div>
-        <div class="page-services__item">
-          <Service url="https://ru.vuejs.org/" title="Vue" text="Vue (произносится /vjuː/, примерно как view) — это прогрессивный фреймворк для создания пользовательских интерфейсов. В отличие от фреймворков-монолитов, Vue создан пригодным для постепенного внедрения.">
-            <img src="/svg/vue.svg" alt="">
-          </Service>
-        </div>
+    <section-header title="Мои преимущества" subtitle="Услуги"/>
+    <div class="page-services__wrap">
+      <div class="page-services__item" v-for="{title,text,icon,url} in servicesText" :key="title">
+        <Service :url="url" :title="title" :text="text">
+          <img :src="icon" alt="">
+        </Service>
       </div>
+    </div>
   </div>
 </template>
 <script>
 import Service from "@/elements/Service";
 import SectionHeader from "@/ui/SectionHeader";
+import services_text from "~/text/services";
 
 export default {
   components: {Service, SectionHeader},
-  layout: 'default'
+  layout: 'default',
+  computed: {
+    servicesText() {
+      return services_text;
+    }
+  }
 }
 </script>
 <style lang="scss">
@@ -49,7 +35,7 @@ export default {
   }
   &__item {
     padding: 4rem;
-    box-shadow: 0 4px 14px -2px rgba(0,0,0,.4);
+    box-shadow: 0 4px 14px -2px rgba(0, 0, 0, .4);
     border-radius: 1rem;
     overflow: hidden;
   }
